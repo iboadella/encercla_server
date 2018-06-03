06ea9a944e91f7e336d8e022d9d2a4f93d3252c4
ericanoanira <irisboadella@gmail.com>
2018-06-03 22:47:56 +0200

    small import

--- resources.py
+++ resources.py
@@ -1,6 +1,6 @@
 from flask_restful import Resource, reqparse
 from flask_jwt_extended import (create_access_token, create_refresh_token, jwt_required, jwt_refresh_token_required, get_jwt_identity, get_raw_jwt)
-
+from models import UserModel
 parser = reqparse.RequestParser()
 parser.add_argument('username', help = 'This field cannot be blank', required = True)
 parser.add_argument('password', help = 'This field cannot be blank', required = True)
